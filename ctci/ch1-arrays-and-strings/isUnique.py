be0ad23152fe0c8be971cc809f8fee0ca6c48ead
Vinz Angelo Madrigal <mvinzangelo@gmail.com>
2022-06-28 08:45:32 -0700

    Add gitignore

--- ctci/ch1-arrays-and-strings/isUnique.py
+++ ctci/ch1-arrays-and-strings/isUnique.py
@@ -1,8 +1,10 @@
+# 1.1 Implement an algorithm to determine if a string has all uque characters. What if you can't use additional data structures?
 def isUnique(string):
     myset = set()
     for i in range(0, len(string)):
         myset.add(string[i])
     return len(myset) == len(string)
 
-print(isUnique("helo"))
+print(isUnique("hello"))
+
 
