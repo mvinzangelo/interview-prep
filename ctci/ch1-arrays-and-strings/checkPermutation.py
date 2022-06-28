13531c7766a761bd0ca48e3bcbaa7102f7dd9310
Vinz Angelo Madrigal <mvinzangelo@gmail.com>
2022-06-28 08:48:53 -0700

    Finish checkPermutation

--- ctci/ch1-arrays-and-strings/checkPermutation.py
+++ ctci/ch1-arrays-and-strings/checkPermutation.py
@@ -1,3 +1,12 @@
 # 1.2 Given two strings, write a method to decide if one is a permutation of the other.
 
+def checkPerm(stringOne, stringTwo):
+    hashOne = 0
+    for i in stringOne:
+        hashOne += ord(i)
+    hashTwo = 0
+    for j in stringTwo:
+        hashTwo += ord(j)
+    return hashOne == hashTwo
 
+print(checkPerm("hello", "helo"))
