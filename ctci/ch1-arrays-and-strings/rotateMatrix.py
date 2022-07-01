77d75df1189e3a02139921d5d630b7c4d85071c4
Vinz Angelo Madrigal <mvinzangelo@gmail.com>
2022-07-01 08:49:36 -0700

    Progress rotateMatrix.py

--- ctci/ch1-arrays-and-strings/rotateMatrix.py
+++ ctci/ch1-arrays-and-strings/rotateMatrix.py
@@ -12,16 +12,17 @@
 def rotate(inMatrix):
 	tempVals = [0] * 4
 	for i in range(0, int(len(inMatrix) / 2)):
-		tempVals[0] = inMatrix[0][i]
-		tempVals[1] = inMatrix[i][len(inMatrix) - 1] 
-		tempVals[2] = inMatrix[len(inMatrix) - 1 - i][0] 
-		tempVals[3] = inMatrix[len(inMatrix) - 1][len(inMatrix) - 1 - i]
+		for j in range(0, int(len(inMatrix) / 2)):
+			tempVals[0] = inMatrix[i][j]
+			tempVals[1] = inMatrix[j][len(inMatrix) - 1 - i] 
+			tempVals[2] = inMatrix[len(inMatrix) - 1 - j][i] 
+			tempVals[3] = inMatrix[len(inMatrix) - 1 - i][len(inMatrix) - 1 - j]
 		
 		
-		inMatrix[0][i] = tempVals[2]
-		inMatrix[i][len(inMatrix) - 1] = tempVals[0]
-		inMatrix[len(inMatrix) - 1 - i][0] = tempVals[3]
-		inMatrix[len(inMatrix) - 1][len(inMatrix) - 1 - i] = tempVals[1]
+			inMatrix[i][j] = tempVals[2]
+			inMatrix[j][len(inMatrix) - 1 - i] = tempVals[0]
+			inMatrix[len(inMatrix) - 1 - j][i] = tempVals[3]
+			inMatrix[len(inMatrix) - 1 - i][len(inMatrix) - 1 - j] = tempVals[1]
 		
 		for j in inMatrix:
 			print(j)
@@ -32,3 +33,4 @@ def rotate(inMatrix):
 rotate([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]])
 # print(rotate([[1,2,3],[4,5,6],[7,8,9]]))
 # print(rotate([[1,2],[3,4]]))
+
